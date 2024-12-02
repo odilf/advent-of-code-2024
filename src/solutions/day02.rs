a9a1f4a9d7090747c0631b4206198b3eaca417f0
Odilf <odysseas.maheras@gmail.com>
2024-12-02 08:34:17 +0100

    feat: day02

--- src/solutions/day02.rs
+++ src/solutions/day02.rs
@@ -1,17 +1,62 @@
-#[elvish::solution(day = 2, example = 142)]
-fn part1(input: &str) -> u32 {
-    todo!();
+fn parse_line(line: &str) -> Vec<i32> {
+    line.split(" ")
+        .map(str::parse::<i32>)
+        .collect::<Result<Vec<_>, _>>()
+        .unwrap()
 }
 
-#[elvish::solution(day = 2, example = 281)]
-fn part2(input: &str) -> u32 {
-    todo!()
+#[elvish::solution(day = 2, example = 2)]
+fn part1(input: &str) -> usize {
+    input
+        .lines()
+        .filter(|&line| {
+            let digits = parse_line(line);
+            valid_list(&digits, (digits[1] - digits[0]).signum())
+        })
+        .count()
+}
+
+fn valid_pair(a: i32, b: i32, signum: i32) -> bool {
+    let diff = b - a;
+
+    diff.abs() >= 1 && diff.abs() <= 3 && diff.signum() == signum
+}
+
+fn valid_list(digits: &[i32], signum: i32) -> bool {
+    digits
+        .windows(2)
+        .all(|slice| valid_pair(slice[0], slice[1], signum))
+}
+
+#[elvish::solution(day = 2, example = 4)]
+fn part2(input: &str) -> usize {
+    input
+        .lines()
+        .filter(|&line| {
+            let digits = parse_line(line);
+
+            (0..=digits.len()).any(|exclude| {
+                let digits: Vec<_> = digits
+                    .iter()
+                    .cloned()
+                    .enumerate()
+                    .filter(|&(i, _)| i != exclude)
+                    .map(|(_, v)| v)
+                    .collect();
+
+                valid_list(&digits, (digits[1] - digits[0]).signum())
+            })
+        })
+        .count()
 }
 
 elvish::example!(
-    part1: "
-    ", 
-
-    part2: "
-    ",
+    "
+        7 6 4 2 1
+        1 2 7 8 9
+        9 7 6 2 1
+        1 3 2 4 5
+        8 6 4 4 1
+        1 3 6 7 9
+    "
 );
