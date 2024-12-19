ed7e7a7404d8dbbb3a4b70b6a5d5ab12894f9bdc
Odilf <odysseas.maheras@gmail.com>
2024-12-19 20:10:28 +0200

    feat: solve day19

--- src/solutions/day19.rs
+++ src/solutions/day19.rs
@@ -1,17 +1,121 @@
-#[elvish::solution(day = 19, example = 142)]
-fn part1(input: &str) -> u32 {
-    todo!();
+use std::collections::{HashMap, VecDeque};
+
+use winnow::{
+    ascii::{alpha1, multispace1},
+    combinator::separated,
+    seq, PResult, Parser,
+};
+
+type Pattern = Vec<Color>;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+enum Color {
+    White,
+    Blue,
+    Black,
+    Red,
+    Green,
 }
 
-#[elvish::solution(day = 19, example = 281)]
-fn part2(input: &str) -> u32 {
-    todo!()
+impl Color {
+    fn from_char(char: char) -> Self {
+        match char {
+            'w' => Self::White,
+            'u' => Self::Blue,
+            'b' => Self::Black,
+            'r' => Self::Red,
+            'g' => Self::Green,
+            _ => panic!("Invalid char {char}"),
+        }
+    }
+}
+
+fn ways_to_make<'p>(
+    pattern: &'p [Color],
+    extensions: &[Pattern],
+    cache: &mut HashMap<&'p [Color], usize>,
+) -> usize {
+    if let Some(&output) = cache.get(pattern) {
+        return output;
+    }
+
+    if pattern.is_empty() {
+        return 1;
+    }
+
+    let output = extensions
+        .into_iter()
+        .filter_map(|extension| {
+            if extension.len() > pattern.len() {
+                return None;
+            }
+            let slice = &pattern[pattern.len() - extension.len()..pattern.len()];
+            if extension.as_slice() == slice {
+                Some(ways_to_make(
+                    &pattern[0..pattern.len() - extension.len()],
+                    extensions,
+                    cache,
+                ))
+            } else {
+                None
+            }
+        })
+        .sum();
+
+    cache.insert(pattern, output);
+    output
+}
+
+#[elvish::solution(day = 19, example = 6)]
+fn part1(input: &str) -> usize {
+    let (available_extensions, target_patterns) = parse(input);
+
+    let mut cache = HashMap::new();
+    target_patterns
+        .iter()
+        .filter(|&pattern| ways_to_make(pattern, &available_extensions, &mut cache) > 0)
+        .count()
+}
+
+#[elvish::solution(day = 19, example = 16)]
+fn part2(input: &str) -> usize {
+    let (available_extensions, target_patterns) = parse(input);
+
+    let mut cache = HashMap::new();
+    target_patterns
+        .iter()
+        .map(|pattern| ways_to_make(pattern, &available_extensions, &mut cache))
+        .sum()
+}
+
+fn parse(input: &str) -> (Vec<Pattern>, Vec<Pattern>) {
+    let pat = |input: &mut &str| -> PResult<Pattern> {
+        alpha1
+            .map(|pat: &str| pat.chars().map(Color::from_char).collect::<Vec<_>>())
+            .parse_next(input)
+    };
+
+    let mut parser = seq!((
+        separated(0.., pat, ", "),
+        _: multispace1,
+        separated(0.., pat, '\n'),
+        _: '\n'
+    ));
+
+    parser.parse(input).unwrap()
 }
 
 elvish::example!(
-    part1: "
-    ", 
+    "
+        r, wr, b, g, bwu, rb, gb, br
 
-    part2: "
-    ",
+        brwrr
+        bggr
+        gbbr
+        rrbgbr
+        ubwu
+        bwurrg
+        brgr
+        bbrgwb
+    "
 );
